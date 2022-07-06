66c9aa86e3f58a82faf16ab3cc853d67d1234f3c
atxr <alexandre.tullot@gmail.com>
2022-07-06 14:54:52 +0200

    Test hashToCurve
    The test compile and the point generation work.
    However, segfault at the end of the program while destructing the Group.

--- test/app.cpp
+++ test/app.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <openssl/ec.h>
+#include "core/Core.hpp"
 
 using namespace std;
 
@@ -7,6 +8,10 @@ int main() {
     cout << "Hello, World!" << std::endl;
     Group G;
     BigInt i;
+    G.get_rand_bn(i);
 
+    Point p = Core::hashToCurve(G, i);
+
+    // TODO: ERROR HERE when destructing the group G
     return 0;
 }
