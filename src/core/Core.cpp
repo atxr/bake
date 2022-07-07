38c9c9f20691e7976d69cf4e8ffc38b914a3ff0c
atxr <alexandre.tullot@gmail.com>
2022-07-07 14:53:16 +0200

    Add blind and unblind to Core

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -5,7 +5,7 @@ void Core::hashToCurve(Point *ret, Group G, BigInt hash)
 {
     *ret = Point();
     ret->fromHash(&G, hash);
-    
+
     if (ret->is_on_curve())
     {
         std::cout << "HashToCurve generated a point p on the curve" << std::endl;
@@ -14,6 +14,20 @@ void Core::hashToCurve(Point *ret, Group G, BigInt hash)
     {
         std::cout << "HashToCurve didn't generate a point on the curve" << std::endl;
     }
+}
 
-    return point;
+Point Core::blind(const Point p, const BigInt r)
+{
+    Point b(p);
+    b = b.mul(r);
+    return b;
+}
+
+Point Core::unblind(const Point b, const BigInt r)
+{
+    Point p(b);
+    BigInt inv_r(r);
+    inv_r = r.inv_mod(b.group->order, b.group->bn_ctx);
+    p = p.mul(inv_r);
+    return p;
 }
--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -1,10 +1,12 @@
-#include <openssl/ec.h> 
-#include <openssl/bn.h> 
-#include <openssl/obj_mac.h> 
+#include <openssl/ec.h>
+#include <openssl/bn.h>
+#include <openssl/obj_mac.h>
 #include "ec.hpp"
 
 class Core
 {
 public:
     static void hashToCurve(Point *ret, Group G, BigInt hash);
+    static Point blind(const Point p, const BigInt r);
+    static Point unblind(const Point b, const BigInt r);
 };
