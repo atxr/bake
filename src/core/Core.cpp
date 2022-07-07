2cec7fad0b4b8e85df8759e929148b3d1c94ca74
atxr <alexandre.tullot@gmail.com>
2022-07-07 11:46:44 +0200

    Add curve test in hashToCurve
    The hashToCruve method test if the point is on the curve

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -1,7 +1,19 @@
 #include "Core.hpp"
+#include <iostream>
 
-Point Core::hashToCurve(Group G, BigInt hash) {
+Point Core::hashToCurve(Group G, BigInt hash)
+{
     Point point;
     point.fromHash(&G, hash);
+    
+    if (point.is_on_curve())
+    {
+        std::cout << "HashToCurve generated a point p on the curve" << std::endl;
+    }
+    else
+    {
+        std::cout << "HashToCurve didn't generate a point on the curve" << std::endl;
+    }
+
     return point;
 }
