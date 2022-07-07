49868605ac83431b467cd08c2dd350d80f33396f
atxr <alexandre.tullot@gmail.com>
2022-07-07 14:53:59 +0200

    Add blind/unblind to the test app

--- test/app.cpp
+++ test/app.cpp
@@ -5,13 +5,32 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
     Group G;
-    BigInt i;
-    G.get_rand_bn(i);
+    BigInt hash, r;
+    G.get_rand_bn(hash);
+    G.get_rand_bn(r);
 
-    Point p = Core::hashToCurve(G, i);
+    Point p;
+    p.fromHash(&G, hash);
+    
+    if (p.is_on_curve())
+    {
+        std::cout << "HashToCurve generated a point p on the curve" << std::endl;
+    }
+    else
+    {
+        std::cout << "HashToCurve didn't generate a point on the curve" << std::endl;
+    }
 
-    // TODO: ERROR HERE when destructing the group G
+    Point b = Core::blind(p, r);
+    Point u = Core::unblind(b, r);
+    if (p != b) {
+        cout << "Blinding OK" << endl;
+    }
+
+    if (p == u) {
+        cout << "Unblinding OK" << endl;
+    }
+
     return 0;
 }
