c3cce003afa3754cae1ce0844b2ddd8e7657d960
atxr <alexandre.tullot@gmail.com>
2022-07-11 12:40:35 +0200

    Add curveToHash function
    Extend the Core class with a method to hash a point into a BigInt

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -1,5 +1,6 @@
 #include "Core.hpp"
 #include <iostream>
+#include <openssl/sha.h>
 
 void Core::hashToCurve(Point *ret, Group G, BigInt hash)
 {
@@ -14,6 +15,26 @@ void Core::hashToCurve(Point *ret, Group G, BigInt hash)
     {
         std::cout << "HashToCurve didn't generate a point on the curve" << std::endl;
     }
+}
+
+BigInt Core::curveToHash(Point p) {
+    BigInt x, y;
+    EC_POINT_get_affine_coordinates(p.group->ec_group, p.point, x.n, y.n, p.group->bn_ctx);
+    unsigned char xbin[BN_num_bytes(x.n)];
+    unsigned char ybin[BN_num_bytes(y.n)];
+    BN_bn2bin(x.n, xbin);
+    BN_bn2bin(y.n, ybin);
+    
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA224_Update(&sha256, xbin, sizeof(xbin));
+    SHA224_Update(&sha256, ybin, sizeof(ybin));
+    SHA256_Final(hash, &sha256);
+
+    BigInt ret;
+    BN_bin2bn(hash, sizeof(hash), ret.n);
+    return ret;
 }
 
 Point Core::blind(const Point p, const BigInt r)
--- src/core/Core.hpp
+++ src/core/Core.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <openssl/ec.h>
 #include <openssl/bn.h>
 #include <openssl/obj_mac.h>
@@ -7,6 +9,7 @@ class Core
 {
 public:
     static void hashToCurve(Point *ret, Group G, BigInt hash);
+    static BigInt curveToHash(Point p);
     static Point blind(const Point p, const BigInt r);
     static Point unblind(const Point b, const BigInt r);
 };
