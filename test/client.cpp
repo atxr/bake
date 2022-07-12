2ebb28d55e0edcd74efdcef36e678beca866b357
atxr <alexandre.tullot@gmail.com>
2022-07-12 09:39:44 +0200

    Correct minor issue in client app

--- test/client.cpp
+++ test/client.cpp
@@ -7,16 +7,13 @@ using namespace std;
 int main() {
     Group G;
     // Init the servers
-    AuthenticationServer as;
+    AuthenticationServer as(G);
     ComputationServer cs(as);
 
     // Create a client
     Client c(0, FuzzyVault()); 
     if (c.enroll(cs)) {
         cout << "Enrollment successful" << endl;
-    }
-    else {
-        cout << "nani" << endl;
     }
     return 0;
 }
