6f502f43f49304d4ef805810a67ce283c7e4e1a9
atxr <alexandre.tullot@gmail.com>
2022-08-15 14:44:51 +0200

    Add better output

--- test/computeTiming.py
+++ test/computeTiming.py
@@ -4,13 +4,18 @@ import glob
 from statistics import median 
 from parse import parse
 
-times = glob.glob("../out/*.chrono")
+times = glob.glob("out/*.chrono")
 
 for path in times:
     t = open(path).readlines()
     t = [int(x) for x in t]
     m = median(t)
-    s = parse("../out/{}.chrono", path)[0]
-    print(f"{path}: {m} microseconds")
+    s = parse("out/{}.chrono", path)[0] + ":"
+
+    if m < 1000:
+        print(f"{s.ljust(10)}{str(m).rjust(7)} μs with {str(len(t)).rjust(4)} samples")
+    else:
+        print(f"{s.ljust(10)}{str(m/1000).rjust(7)} ms with {str(len(t)).rjust(4)} samples")
+
     
     
